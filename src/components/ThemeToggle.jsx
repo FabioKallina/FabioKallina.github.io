d21c97425473cc187d986b8eb75487e355341f5b
FabioKallina <fabiokdp8@gmail.com>
2025-06-14 18:44:15 -0700

    Added toggle theme button to small screens

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,7 +33,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-2 right-5 z-50 p-2 rounded-full transition-colors duration-300 cursor-pointer",
+        "fixed top-2 right-2 max-sm:top-1 max-sm:right-1 z-50 p-2 rounded-full transition-colors duration-300 cursor-pointer",
         "focus: outline-hidden"
       )}
     >
